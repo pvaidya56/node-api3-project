03e3e95eb30e748d886bab0a1a9b53a3b761a1ea
Priya <pvaidya56@gmail.com>
2020-03-12 02:39:44 -0700

    MVP Complete

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+server.use(logger); 
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const { ip, method, url} = req;
+
+  console.log(`${ip} ${method} ${url}`)
+  next(); //to pass control to the next middleware function
+}
 
 module.exports = server;
