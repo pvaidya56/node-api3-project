eb14939ac6d2137e2d99d9d9fecea6bc71cf970b
Priya <pvaidya56@gmail.com>
2020-03-12 15:12:25 -0700

    port env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // require("dotenv").config()
 const server = require("./server");
 
-const port = 5000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
     console.log(`\n*** Server running on http://localhost:${port} ***\n`);
